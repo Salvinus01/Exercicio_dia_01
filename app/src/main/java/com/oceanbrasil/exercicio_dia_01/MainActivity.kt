8e164d3ffb62a83fbc298774560a5ca12559a241
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-21 18:12:21 -0300

    Fim

--- app/src/main/java/com/oceanbrasil/exercicio_dia_01/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/exercicio_dia_01/MainActivity.kt
@@ -2,10 +2,31 @@ package com.oceanbrasil.exercicio_dia_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etNome = findViewById<EditText>(R.id.etNome)
+        val etSobreNome = findViewById<EditText>(R.id.etSobreNome)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btLimpar = findViewById<Button>(R.id.btLimpar)
+
+        btEnviar.setOnClickListener {
+            tvResultado.text = String.format("Bem Vindo %1s %2s", etNome.text.toString(), etSobreNome.text.toString())
+        }
+
+        btLimpar.setOnClickListener {
+            etNome.setText("")
+            etSobreNome.setText("")
+            tvResultado.text = ""
+            etNome.requestFocus()
+        }
+
     }
 }
